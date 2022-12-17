2a7be1cffb55ce7f07ccb70413a58085350d5aae
shotaro-ada <syouta2223505@gmail.com>
2022-12-17 16:12:53 +0900

    毎月一日に関数を実行するスクリプト完成

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import json
 import requests
+import datetime
+import time
 
 
 app = Flask(__name__)
@@ -8,39 +10,58 @@ token = "xoxb-597500547424-4511524546932-o3RDC4fjQtLnQOvDn1RVhTz8"
 headers = {"Authorization": "Bearer " + token}
 
 
-#ワークスペース内の全チャンネルのID取得
+
+
+# ワークスペース内の全チャンネルのID取得
 def get_channel_id():
     url = 'https://slack.com/api/conversations.list'
     r = requests.get(url, headers=headers)
     channel_data = r.json()
     channels = channel_data["channels"]
-    channel_id_list = []      #この配列にIDを格納
-    for i in channel_data["channels"]:
+    channel_id_list = []  # この配列にIDを格納
+    for i in channels:
         channel_id_list.append(i["id"])
-    return channel_id_list #配列を返す
+    print(channel_id_list)
+    return channel_id_list  # 配列を返す
 
 
-
-
-#チャンネルからメッセージを取得
-def get_messages(id):
-    url = 'https://slack.com/api/conversations.history'    
+def send_message():
+    url = "https://slack.com/api/chat.postMessage"
     data = {
-        "channel": id, #適当な変数(チャンネルid)に後で変える
-        'include_all_metadata': True
+        'channel': "C03CR11BLNS",
+        'text': "botが送れる文字数のテスト"
+    }
+    r = requests.post(url, headers=headers, data=data)
+    print(r.json())
+
+
+
+# チャンネルからメッセージを取得
+def get_messages(id,ts):
+    url = 'https://slack.com/api/conversations.history'
+    data = {
+        "channel": id, 
+        "include_all_metadata": True,
+        "oldest": ts
     }
     r = requests.get(url, headers=headers, params=data)
     history = r.json()
     messages = history["messages"]
-    messages.reverse()      #messages配列の順番を逆にした
-    messages_json = json.dumps(messages, ensure_ascii=False, indent=4)   
-    print(messages_json)
+    # messages配列の順番を逆にする
+    messages.reverse()
+    messages_json = json.dumps(messages, ensure_ascii=False, indent=4)
     # with open('test.json', 'w') as f:
     #     json.dump(messages, f, ensure_ascii=False, indent=4)
     return messages_json
 
 
-#メッセージのリプライを取得
+#データサーバーに送るためにmessagesとrepliesをいい感じにまとめる
+def format_messages():
+
+    return ""
+
+
+# メッセージのリプライを取得
 def get_replies():
     url = 'https://slack.com/api/conversations.replies'
     data = {
@@ -50,37 +71,55 @@ def get_replies():
     r = requests.get(url, headers=headers, params=data)
     replies = r.json()
     replies = replies['messages']
-    replies_json = json.dumps(replies, ensure_ascii=False, indent=4)   
-    print(replies_json)
+    replies_json = json.dumps(replies, ensure_ascii=False, indent=4)
+    with open('replies.json', 'w') as f:
+        json.dump(replies, f, ensure_ascii=False, indent=4)
     return replies
 
 
-
-#よくわからん機能
+# よくわからん機能
 def get_archive():
     url = 'https://slack.com/api/conversations.archive'
     data = {
         "channel": "C03CR11BLNS"
     }
-    r = requests.post(url, headers=headers, data=data)  
+    r = requests.post(url, headers=headers, data=data)
     archive = r.json()
     print(archive)
     return archive
 
 
-@app.route('/', methods=["GET"])
-def return_messages():
-    return get_replies()
-    
+# @app.route('/', methods=["GET"])
+# def return_messages():
+#     return get_replies()
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run(debug=True, host='0.0.0.0', port=8090)
-    
+
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run(debug=True, host='0.0.0.0', port=8090)
+
 # https://infinite-earth-07156.herokuapp.com
 
 
-
-#関数をfor文で回したら全チャンネルのメッセージとれるかも
+# 関数をfor文で回したら全チャンネルのメッセージとれるかも
 # for id in get_channel_id():
 #     get_messages(id)
+
+
+
+while True:
+    now = datetime.datetime.now()
+    target = 17
+
+    if now.day == target:
+        ts = int(time.time()) #現在のunix時間
+        #ここですべての関数を呼び出す
+
+        while now.day == target:
+            time.sleep(10)
+
+    time.sleep(10)
+ 
+
+
+get_replies()
